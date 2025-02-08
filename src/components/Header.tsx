e19da1a205ade4a823e8a3cd1ff6139df2b1f8b3
SKneha0 <160652461+SKneha0@users.noreply.github.com>
2025-02-08 19:37:26 +0500

    dashboard

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+// import { UserButton } from "@clerk/nextjs"
+const Header = () => {
+  return (
+    <header className="bg-white shadow-md py-4 px-4">
+      <div className="flex items-center justify-center">
+        <div className="flex items-center">
+          {/* <UserButton /> */}
+         </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
